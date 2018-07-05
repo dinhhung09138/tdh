415edf9b489cc153bcb1c761f9050452e6d83014
Anthony <dinhhung09138@gmail.com>
2018-07-05 11:08:17 +0700

    Update model from database

--- TDH/Models/TDHModel.Context.cs
+++ TDH/Models/TDHModel.Context.cs
@@ -45,5 +45,15 @@ namespace TDH.Models
         public virtual DbSet<REPORT_COMMENT> REPORT_COMMENT { get; set; }
         public virtual DbSet<TARGET> TARGETs { get; set; }
         public virtual DbSet<TARGET_TASK> TARGET_TASK { get; set; }
+        public virtual DbSet<MM_ACCOUNT> MM_ACCOUNT { get; set; }
+        public virtual DbSet<MM_ACCOUNT_HISTORY> MM_ACCOUNT_HISTORY { get; set; }
+        public virtual DbSet<MM_ACCOUNT_TYPE> MM_ACCOUNT_TYPE { get; set; }
+        public virtual DbSet<MM_INCOME> MM_INCOME { get; set; }
+        public virtual DbSet<MM_PAYMENT> MM_PAYMENT { get; set; }
+        public virtual DbSet<MM_TRANSFER> MM_TRANSFER { get; set; }
+        public virtual DbSet<MN_CATEGORY> MN_CATEGORY { get; set; }
+        public virtual DbSet<MN_CATEGORY_SETTING> MN_CATEGORY_SETTING { get; set; }
+        public virtual DbSet<MN_GROUP> MN_GROUP { get; set; }
+        public virtual DbSet<MN_GROUP_SETTING> MN_GROUP_SETTING { get; set; }
     }
 }

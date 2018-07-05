85c524810a926bbce2721b3bc4dfbce1a9a62241
Anthony <dinhhung09138@gmail.com>
2018-07-05 17:32:00 +0700

    Account type form

---
+++ TDH/Areas/Administrator/Content/custom/admmoney.accounttype.js
@@ -0,0 +1,193 @@
+﻿var table;
+var id = 0;
+
+$(document).ready(function () {
+    var allowEdit = $('#edit').val();
+    var allowDelete = $('#delete').val();
+    //
+    table = $('#tbList').DataTable({
+        processing: true,
+        serverSide: true,
+        searching: true,
+        ordering: true,
+        paging: true,
+        responsive: true,
+        pageLength: 10,
+        pagingType: 'full_numbers',
+        info: true,
+        autoWidth: false,
+        initComplete: function (settings, json) {
+            //Do something after finish
+        },
+        language: language,
+        order: [[2, "asc"]],
+        ajax: {
+            url: '/administrator/admmoney/accounttype',
+            type: 'post',
+            data: function (d) {
+                //d.ModuleCode = ""
+            }
+        },
+        columns: [
+            {
+                orderable: false,
+                width: '40px',
+                className: 'ctn-center',
+                render: function (obj, type, data, meta) {
+                    return meta.row + meta.settings._iDisplayStart + 1;
+                }
+            },
+            {
+                data: 'Name',
+                orderable: true,
+                searchable: true,
+                width: '200px'
+            },
+            {
+                data: 'Ordering',
+                orderable: true,
+                searchable: true,
+                className: 'ctn-center',
+                width: '90px'
+            },
+            {
+                data: 'CountString',
+                orderable: false,
+                searchable: false,
+                className: 'ctn-center',
+                width: '90px'
+            },
+            {
+                data: 'Publish',
+                orderable: false,
+                searchable: false,
+                className: 'ctn-center',
+                width: '60px',
+                render: function (obj, type, data, meta) {
+                    if (allowEdit === 'True' && data.Count === 0) {
+                        if (data.Publish === true) {
+                            return '<input type="checkbox" class="flat" name="publish" checked  value="' + data.ID + '" />';
+                        } else {
+                            return '<input type="checkbox" class="flat" name="publish" value="' + data.ID + '" />';
+                        }
+                    } else {
+                        if (data.Publish === true) {
+                            return '<div class="icheckbox_flat-green checked" style="position: relative;">\
+                                        <input type="checkbox" class="flat" name="table_records" checked="" \
+                                               style="position: absolute; top: -20%; left: -20%; display: block; width: 140%; height: 140%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;">\
+                                        <ins class="iCheck-helper" \
+                                                style="position: absolute; top: -20%; left: -20%; display: block; width: 140%; height: 140%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;">\
+                                        </ins>\
+                                    </div>';
+                        } else {
+                            return '<div class="icheckbox_flat-green" style="position: relative;">\
+                                        <input type="checkbox" class="flat" name="table_records" value="14" \
+                                               style="position: absolute; top: -20%; left: -20%; display: block; width: 140%; height: 140%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;">\
+                                        <ins class="iCheck-helper" \
+                                               style="position: absolute; top: -20%; left: -20%; display: block; width: 140%; height: 140%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;">\
+                                        </ins>\
+                                    </div>';
+                        }
+                    }
+                }
+            },
+            {
+                orderable: false,
+                width: '40px',
+                className: 'ctn-center',
+                render: function (obj, type, data, meta) {
+                    var str = '';
+                    if (allowEdit === "True") {
+                        str = str + '<a href="javascript:;" data-url="/administrator/admmoney/editaccounttype/' + data.ID + '\" data-title="Cập nhật loại tài khoản" title="Cập nhật" class="pg_ld"><i class="fa fa-edit" aria-hidden="true"></i></a>';
+                    }
+                    if (allowDelete === "True") {
+                        str = str + '<a href="javascript:;" title="Xóa" onclick="confirmDelete(\'' + data.ID + '\');"><i class="fa fa-remove" aria-hidden="true"></i></a>';
+                    }
+                    return str;
+                }
+            }
+        ]
+    });
+
+    table.on('draw', function () {
+        if ($('#tbList input[name="publish"]')[0]) {
+            $('#tbList input[name="publish"]').iCheck({
+                checkboxClass: 'icheckbox_flat-green',
+                radioClass: 'iradio_flat-green',
+                increaseArea: '20%'
+            });
+
+            $('#tbList input[name="publish"]').on('ifChecked', function () {
+                savePublish($(this).val(), true);
+            });
+
+            $('#tbList input[name="publish"]').on('ifUnchecked', function () {
+                savePublish($(this).val(), false);
+            });
+
+        }
+    });
+});
+
+function savePublish(id, publish) {
+    $.ajax({
+        url: '/administrator/admmoney/publishaccounttype',
+        type: 'POST',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({ ID: id, Publish: publish }),
+        success: function (response) {
+            if (response === 0) {
+                table.ajax.reload();
+            }
+            id = '';
+        },
+        error: function (xhr, status, error) {
+            console.log(error);
+        }
+    });
+}
+
+function confirmDelete(deletedId) {
+    $.ajax({
+        url: '/administrator/admmoney/checkdeleteaccounttype',
+        type: 'POST',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({ ID: deletedId }),
+        success: function (response) {
+            if (response === 3) {
+                id = deletedId;
+                $('#deleteModal').modal('show');
+            } else {
+                id = '';
+            }
+            $('#deleteModal').modal('hide');
+        },
+        error: function (xhr, status, error) {
+            console.log(error);
+            $('#deleteModal').modal('hide');
+        }
+    });
+}
+
+function deleteItem() {
+    $.ajax({
+        url: '/administrator/admmoney/deleteaccounttype',
+        type: 'POST',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({ ID: id }),
+        success: function (response) {
+            if (response === 0) {
+                table.ajax.reload();
+            }
+            id = '';
+            $('#deleteModal').modal('hide');
+        },
+        error: function (xhr, status, error) {
+            console.log(error);
+            $('#deleteModal').modal('hide');
+        }
+    });
+}
---
+++ TDH/Areas/Administrator/Services/MoneyAccountService.cs
@@ -0,0 +1,418 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TDH.Models;
+using Utils.JqueryDatatable;
+using TDH.Areas.Administrator.Models;
+using Utils;
+using TDH.Areas.Administrator.Common;
+
+namespace TDH.Areas.Administrator.Services
+{
+    public class MoneyAccountService
+    {
+        #region " [ Properties ] "
+
+        /// <summary>
+        /// File name
+        /// </summary>
+        private readonly string FILE_NAME = "Administrator/Services/MoneyAccountService.cs";
+
+        #endregion
+
+        /// <summary>
+        /// Get list data using jquery datatable
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="userID">User id</param>
+        /// <returns><string, object></returns>
+        public Dictionary<string, object> List(CustomDataTableRequestHelper request, Guid userID)
+        {
+            Dictionary<string, object> _return = new Dictionary<string, object>();
+            try
+            {
+                //Declare response data to json object
+                DataTableResponse<MoneyAccountModel> _itemResponse = new DataTableResponse<MoneyAccountModel>();
+                //List of data
+                List<MoneyAccountModel> _list = new List<MoneyAccountModel>();
+                using (var context = new chacd26d_trandinhhungEntities())
+                {
+                    var _lData = (from m in context.MN_ACCOUNT
+                                  join n in context.MN_ACCOUNT_TYPE on m.account_type_id equals n.id
+                                  where !n.deleted && n.publish && !m.deleted
+                                  orderby m.name descending
+                                  select new
+                                  {
+                                      m.id,
+                                      m.name,
+                                      m.start,
+                                      m.end,
+                                      m.input,
+                                      m.output,
+                                      type_name = n.name
+                                  }).ToList();
+
+                    _itemResponse.draw = request.draw;
+                    _itemResponse.recordsTotal = _lData.Count;
+                    //Search
+                    if (request.search != null && !string.IsNullOrWhiteSpace(request.search.Value))
+                    {
+                        string searchValue = request.search.Value.ToLower();
+                        _lData = _lData.Where(m => m.name.ToLower().Contains(searchValue) ||
+                                                   m.type_name.ToLower().Contains(searchValue)).ToList();
+                    }
+                    //Add to list
+                    foreach (var item in _lData)
+                    {
+                        _list.Add(new MoneyAccountModel()
+                        {
+                            ID = item.id,
+                            Name = item.name,
+                            AccountTypeName = item.type_name,
+                            Start = item.start,
+                            StartString = item.start.NumberToString(),
+                            End = item.end,
+                            EndString = item.end.NumberToString(),
+                            Input = item.input,
+                            InputString = item.input.NumberToString(),
+                            Output = item.output,
+                            OutputString = item.output.NumberToString()
+                        });
+                    }
+                    _itemResponse.recordsFiltered = _list.Count;
+                    IOrderedEnumerable<MoneyAccountModel> _sortList = null;
+                    if (request.order != null)
+                    {
+                        foreach (var col in request.order)
+                        {
+                            switch (col.ColumnName)
+                            {
+                                case "Name":
+                                    _sortList = _sortList == null ? _list.Sort(col.Dir, m => m.Name) : _sortList.Sort(col.Dir, m => m.Name);
+                                    break;
+                                case "AccountTypeName":
+                                    _sortList = _sortList == null ? _list.Sort(col.Dir, m => m.AccountTypeName) : _sortList.Sort(col.Dir, m => m.AccountTypeName);
+                                    break;
+                            }
+                        }
+                        _itemResponse.data = _sortList.Skip(request.start).Take(request.length).ToList();
+                    }
+                    else
+                    {
+                        _itemResponse.data = _list.Skip(request.start).Take(request.length).ToList();
+                    }
+                    _return.Add(DatatableCommonSetting.Response.DATA, _itemResponse);
+                }
+                _return.Add(DatatableCommonSetting.Response.STATUS, ResponseStatusCodeHelper.OK);
+            }
+            catch (Exception ex)
+            {
+                Notifier.Notification(userID, Resources.Message.Error, Notifier.TYPE.Error);
+                TDH.Services.Log.WriteLog(FILE_NAME, "List", userID, ex);
+                throw new ApplicationException();
+            }
+
+            return _return;
+        }
+
+        /// <summary>
+        /// Get all item without deleted
+        /// </summary>
+        /// <returns></returns>
+        public List<MoneyAccountModel> GetAll(Guid userID)
+        {
+            try
+            {
+                List<MoneyAccountModel> _return = new List<MoneyAccountModel>();
+                using (var context = new chacd26d_trandinhhungEntities())
+                {
+                    var _list = (from m in context.MN_ACCOUNT
+                                 join n in context.MN_ACCOUNT_TYPE on m.account_type_id equals n.id
+                                 where !m.deleted && !n.deleted && n.publish && m.publish
+                                 orderby m.name descending
+                                 select new
+                                 {
+                                     m.id,
+                                     m.name
+                                 }).ToList();
+                    foreach (var item in _list)
+                    {
+                        _return.Add(new MoneyAccountModel() { ID = item.id, Name = item.name });
+                    }
+                }
+                return _return;
+            }
+            catch (Exception ex)
+            {
+                Notifier.Notification(userID, Resources.Message.Error, Notifier.TYPE.Error);
+                TDH.Services.Log.WriteLog(FILE_NAME, "GetAll", userID, ex);
+                throw new ApplicationException();
+            }
+        }
+
+        /// <summary>
+        /// Get item
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>MoneyAccountModel. Throw exception if not found or get some error</returns>
+        public MoneyAccountModel GetItemByID(MoneyAccountModel model)
+        {
+            try
+            {
+                using (var context = new chacd26d_trandinhhungEntities())
+                {
+                    MN_ACCOUNT _md = context.MN_ACCOUNT.FirstOrDefault(m => m.id == model.ID && !m.deleted);
+                    if (_md == null)
+                    {
+                        throw new FieldAccessException();
+                    }
+                    MN_ACCOUNT_TYPE _type = context.MN_ACCOUNT_TYPE.FirstOrDefault(m => m.id == _md.account_type_id);
+                    return new MoneyAccountModel()
+                    {
+                        ID = _md.id,
+                        Name = _md.name,
+                        AccountTypeID = _md.account_type_id,
+                        AccountTypeName = _type.name,
+                        Start = _md.start,
+                        End = _md.end,
+                        Input = _md.input,
+                        Output = _md.output
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                Notifier.Notification(model.CreateBy, Resources.Message.Error, Notifier.TYPE.Error);
+                TDH.Services.Log.WriteLog(FILE_NAME, "GetItemByID", model.CreateBy, ex);
+                throw new ApplicationException();
+            }
+        }
+
+        /// <summary>
+        /// Save
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>ResponseStatusCodeHelper</returns>
+        public ResponseStatusCodeHelper Save(MoneyAccountModel model)
+        {
+            try
+            {
+                using (var context = new chacd26d_trandinhhungEntities())
+                {
+                    using (var trans = context.Database.BeginTransaction())
+                    {
+                        try
+                        {
+                            MN_ACCOUNT _md = new MN_ACCOUNT();
+                            if (model.Insert)
+                            {
+                                _md.id = Guid.NewGuid();
+                            }
+                            else
+                            {
+                                _md = context.MN_ACCOUNT.FirstOrDefault(m => m.id == model.ID && !m.deleted);
+                                if (_md == null)
+                                {
+                                    throw new FieldAccessException();
+                                }
+                            }
+                            _md.account_type_id = model.AccountTypeID;
+                            _md.name = model.Name;
+                            //Create or edit, only change the name and type
+                            if (model.Insert)
+                            {
+                                _md.create_by = model.CreateBy;
+                                _md.create_date = DateTime.Now;
+                                context.MN_ACCOUNT.Add(_md);
+                                context.Entry(_md).State = System.Data.Entity.EntityState.Added;
+                            }
+                            else
+                            {
+                                _md.update_by = model.UpdateBy;
+                                _md.update_date = DateTime.Now;
+                                context.MN_ACCOUNT.Attach(_md);
+                                context.Entry(_md).State = System.Data.Entity.EntityState.Modified;
+                            }
+                            context.SaveChanges();
+                            trans.Commit();
+                        }
+                        catch (Exception ex)
+                        {
+                            Notifier.Notification(model.CreateBy, Resources.Message.Error, Notifier.TYPE.Error);
+                            trans.Rollback();
+                            TDH.Services.Log.WriteLog(FILE_NAME, "Save", model.CreateBy, ex);
+                            throw new ApplicationException();
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Notifier.Notification(model.CreateBy, Resources.Message.Error, Notifier.TYPE.Error);
+                TDH.Services.Log.WriteLog(FILE_NAME, "Save", model.CreateBy, ex);
+                throw new ApplicationException();
+            }
+            if (model.Insert)
+            {
+                Notifier.Notification(model.CreateBy, Resources.Message.InsertSuccess, Notifier.TYPE.Success);
+            }
+            else
+            {
+                Notifier.Notification(model.CreateBy, Resources.Message.UpdateSuccess, Notifier.TYPE.Success);
+            }
+            return ResponseStatusCodeHelper.Success;
+        }
+
+        /// <summary>
+        /// Publish
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>ResponseStatusCodeHelper</returns>
+        public ResponseStatusCodeHelper Publish(MoneyAccountModel model)
+        {
+            try
+            {
+                using (var context = new chacd26d_trandinhhungEntities())
+                {
+                    using (var trans = context.Database.BeginTransaction())
+                    {
+                        try
+                        {
+                            MN_ACCOUNT _md = context.MN_ACCOUNT.FirstOrDefault(m => m.id == model.ID && !m.deleted);
+                            if (_md == null)
+                            {
+                                throw new FieldAccessException();
+                            }
+                            _md.publish = model.Publish;
+                            _md.update_by = model.UpdateBy;
+                            _md.update_date = DateTime.Now;
+                            context.MN_ACCOUNT.Attach(_md);
+                            context.Entry(_md).State = System.Data.Entity.EntityState.Modified;
+                            context.SaveChanges();
+                            trans.Commit();
+                        }
+                        catch (Exception ex)
+                        {
+                            Notifier.Notification(model.CreateBy, Resources.Message.Error, Notifier.TYPE.Error);
+                            trans.Rollback();
+                            TDH.Services.Log.WriteLog(FILE_NAME, "Publish", model.CreateBy, ex);
+                            throw new ApplicationException();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Notifier.Notification(model.CreateBy, Resources.Message.Error, Notifier.TYPE.Error);
+                TDH.Services.Log.WriteLog(FILE_NAME, "Publish", model.CreateBy, ex);
+                throw new ApplicationException();
+            }
+            Notifier.Notification(model.CreateBy, Resources.Message.UpdateSuccess, Notifier.TYPE.Success);
+            return ResponseStatusCodeHelper.Success;
+        }
+        
+        /// <summary>
+        /// Delete
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>ResponseStatusCodeHelper</returns>
+        public ResponseStatusCodeHelper Delete(MoneyAccountModel model)
+        {
+            try
+            {
+                using (var context = new chacd26d_trandinhhungEntities())
+                {
+                    using (var trans = context.Database.BeginTransaction())
+                    {
+                        try
+                        {
+                            MN_ACCOUNT _md = context.MN_ACCOUNT.FirstOrDefault(m => m.id == model.ID && !m.deleted);
+                            if (_md == null)
+                            {
+                                throw new FieldAccessException();
+                            }
+                            _md.deleted = true;
+                            _md.delete_by = model.DeleteBy;
+                            _md.delete_date = DateTime.Now;
+                            context.MN_ACCOUNT.Attach(_md);
+                            context.Entry(_md).State = System.Data.Entity.EntityState.Modified;
+                            context.SaveChanges();
+                            trans.Commit();
+                        }
+                        catch (Exception ex)
+                        {
+                            Notifier.Notification(model.CreateBy, Resources.Message.Error, Notifier.TYPE.Error);
+                            trans.Rollback();
+                            TDH.Services.Log.WriteLog(FILE_NAME, "Delete", model.CreateBy, ex);
+                            throw new ApplicationException();
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Notifier.Notification(model.CreateBy, Resources.Message.Error, Notifier.TYPE.Error);
+                TDH.Services.Log.WriteLog(FILE_NAME, "Delete", model.CreateBy, ex);
+                throw new ApplicationException();
+            }
+            Notifier.Notification(model.CreateBy, Resources.Message.DeleteSuccess, Notifier.TYPE.Success);
+            return ResponseStatusCodeHelper.Success;
+        }
+
+        /// <summary>
+        /// Check Delete
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>ResponseStatusCodeHelper</returns>
+        public ResponseStatusCodeHelper CheckDelete(MoneyAccountModel model)
+        {
+            try
+            {
+                using (var context = new chacd26d_trandinhhungEntities())
+                {
+
+                    MN_ACCOUNT _md = context.MN_ACCOUNT.FirstOrDefault(m => m.id == model.ID && !m.deleted);
+                    if (_md == null)
+                    {
+                        throw new FieldAccessException();
+                    }
+                    var _accSetting = context.MN_ACCOUNT_SETTING.FirstOrDefault(m => m.account_id == model.ID && !m.deleted);
+                    if (_accSetting != null)
+                    {
+                        return ResponseStatusCodeHelper.NG;
+                    }
+                    var _payment = context.MN_PAYMENT.FirstOrDefault(m => m.account_id == model.ID && !m.deleted);
+                    if (_payment != null)
+                    {
+                        return ResponseStatusCodeHelper.NG;
+                    }
+                    var _income = context.MN_INCOME.FirstOrDefault(m => m.account_id == model.ID && !m.deleted);
+                    if (_income != null)
+                    {
+                        return ResponseStatusCodeHelper.NG;
+                    }
+                    var _transFrom = context.MN_TRANSFER.FirstOrDefault(m => m.account_from == model.ID && !m.deleted);
+                    if (_transFrom != null)
+                    {
+                        return ResponseStatusCodeHelper.NG;
+                    }
+                    var _transTo = context.MN_TRANSFER.FirstOrDefault(m => m.account_to == model.ID && !m.deleted);
+                    if (_transTo != null)
+                    {
+                        return ResponseStatusCodeHelper.NG;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Notifier.Notification(model.CreateBy, Resources.Message.Error, Notifier.TYPE.Error);
+                TDH.Services.Log.WriteLog(FILE_NAME, "CheckDelete", model.CreateBy, ex);
+                throw new ApplicationException();
+            }
+            return ResponseStatusCodeHelper.OK;
+        }
+
+    }
+}

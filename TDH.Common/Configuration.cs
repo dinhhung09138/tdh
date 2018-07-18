51a6d6abe820d9ea1d2ff8304165d49e5136419c
Anthony <dinhhung09138@gmail.com>
2018-07-18 14:11:32 +0700

    refs #4 Notifier and Configuration services

--- TDH.Common/Configuration.cs
+++ TDH.Common/Configuration.cs
@@ -1,12 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TDH.DataAccess;
 
 namespace TDH.Common
 {
-    class Configuration
+    public class Configuration
     {
+        #region " [ Properties ] "
+
+        /// <summary>
+        /// File name
+        /// </summary>
+        private static readonly string FILE_NAME = "Common/Configuration.cs";
+
+        #endregion
+
+        /// <summary>
+        /// Get config setting value
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string SettingValue(string key)
+        {
+            try
+            {
+                using (var context = new TDHEntities())
+                {
+                    var _item = context.CONFIGURATIONs.FirstOrDefault(m => m.key == key);
+                    if (_item != null)
+                    {
+                        return _item.value;
+                    }
+                    return "";
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.WriteLog(FILE_NAME, "SettingValue", new Guid(), ex);
+                throw new ApplicationException();
+            }
+        }
+
     }
 }
---
+++ TDH.Common/Notifier.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDH.Common
+{
+    public class Notifier
+    {
+        #region " [ Properties ] "
+
+        /// <summary>
+        /// File name
+        /// </summary>
+        private static readonly string FILE_NAME = "Common/Notifier.cs";
+
+        private static string HUB_URL = Configuration.SettingValue("setting_hubconnect");
+
+        private static string HUB_NAME = Configuration.SettingValue("setting_hubname");
+
+        /// <summary>
+        /// Type of notification: success, error, warning
+        /// </summary>
+        public class TYPE
+        {
+            public static readonly string Success = "success";
+            public static readonly string Error = "error";
+            public static readonly string Warning = "warning";
+        }
+
+        #endregion
+
+        public static void Notification(Guid userID, string message, string type)
+        {
+            try
+            {
+                var _hubConnection = new HubConnection(HUB_URL)
+                {
+                    Credentials = CredentialCache.DefaultCredentials,
+                    TraceLevel = TraceLevels.All,
+                    TraceWriter = Console.Out
+                };
+                IHubProxy _hub = _hubConnection.CreateHubProxy(HUB_NAME);
+                _hubConnection.Start().Wait();
+                _hub.Invoke("SendNotification", userID.ToString(), message, type);
+                _hubConnection.Stop();
+
+            }
+            catch (Exception ex)
+            {
+                Log.WriteLog(FILE_NAME, "Notification", userID, ex);
+            }
+        }
+
+        //public static async Task Notification(Guid userID, string message, string type)
+        //{
+        //    try
+        //    {
+        //        var _hubConnection = new HubConnection(HUB_URL)
+        //        {
+        //            Credentials = CredentialCache.DefaultCredentials,
+        //            TraceLevel = TraceLevels.All,
+        //            TraceWriter = Console.Out
+        //        };
+        //        IHubProxy _hub = _hubConnection.CreateHubProxy(HUB_NAME);
+        //        _hubConnection.Start().Wait();
+        //        await _hub.Invoke("SendNotification", userID.ToString(), message, type);
+        //        _hubConnection.Stop();
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        TDH.Services.Log.WriteLog(FILE_NAME, "Notification", userID, ex);
+        //    }
+        //}
+    }
+}

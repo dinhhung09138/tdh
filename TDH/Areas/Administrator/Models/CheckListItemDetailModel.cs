bc4c7cfe4d935048e1586fdafa51a5a17e8236a1
11 <nhinxungquanhminh@gmail.com>
2018-07-20 16:55:15 +0700

    Thêm CheckListItemModel, CheckListItemDetailModel,
    Chỉnh sửa lại CheckListGroupModel

--- TDH/Areas/Administrator/Models/CheckListItemDetailModel.cs
+++ TDH/Areas/Administrator/Models/CheckListItemDetailModel.cs
@@ -6,18 +6,25 @@ using System.Web;
 
 namespace TDH.Areas.Administrator.Models
 {
-    public class CheckListGroup : Utils.Database.BaseModel
+    public class CheckListItemDetailModel: Utils.Database.BaseModel
     {
         public Guid ID { get; set; }
+
+        [Required(ErrorMessage = "GroupID không được rỗng")]
+        public Guid CheckID { get; set; }
 
         [Required(ErrorMessage = "Tiêu đề không được rỗng")]
         [StringLength(100, MinimumLength = 1, ErrorMessage = "Nội dung không quá 100 ký tự")]
         public string title { get; set; }
 
-        public DateTime updateDate { get; set; }
-        [Required]
-        public bool deleted { get; set; }
-        public DateTime deleteDate { get; set; }
+        [Required(ErrorMessage = "Ngày tạo không được rỗng")]
+        public DateTime createDate { get; set; }
 
+        public DateTime updateDate { get; set; }
+
+        [Required(ErrorMessage = "Nội dung không được rỗng")]
+        public bool deleted { get; set; }
+        
+        public DateTime deleteDate { get; set; }
     }
 }

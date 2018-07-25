76c009d9b68a75d957eed04ef956884c45bc37b8
Anthony <dinhhung09138@gmail.com>
2018-07-25 17:43:47 +0700

    refs #5 Add filter into common lib

---
+++ TDH.Common/Fillters/AuthorizationFilterAttribute.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Utils;
+using System.Web.Mvc;
+
+namespace TDH.Common.Fillters
+{
+    /// <summary>
+    /// Authorization filter attribute class
+    /// </summary>
+    public class AuthorizationFilterAttribute : IAuthorizationFilter
+    {
+
+        /// <summary>
+        /// On authorization
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (filterContext == null)
+            {
+                throw new ArgumentNullException();
+            }
+            string area = "";
+            if (filterContext.RouteData.DataTokens["area"] != null)
+            {
+                area = filterContext.RouteData.DataTokens["area"].ToString();
+            }
+            string controllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
+            string actionName = filterContext.ActionDescriptor.ActionName;
+            //Stop execute when action is a child inside or allowAnonymousAttribute
+            bool skipAuth = filterContext.ActionDescriptor.IsDefined(typeof(AllowAnonymousAttribute), inherit: true)
+                || filterContext.ActionDescriptor.ControllerDescriptor.IsDefined(typeof(AllowAnonymousAttribute), inherit: true)
+                || filterContext.IsChildAction;
+            if (skipAuth)
+            {
+                return;
+            }
+
+            var _user = filterContext.HttpContext.Session[CommonHelper.SESSION_LOGIN_NAME] as Utils.CommonModel.UserLoginModel;
+            if (_user != null && _user.UserID.ToString().Length > 0 && _user.UserName.Length > 0)
+            {
+                //Check token
+                filterContext.Result = new HttpUnauthorizedResult();
+                return;
+            }
+            if (filterContext.HttpContext.Request.IsAjaxRequest())
+            {
+                filterContext.Result = new HttpUnauthorizedResult();
+                return;
+            }
+            throw new UnauthorizedAccessException();
+        }
+    }
+}

19a14fc557e20ab116944bcf7a19e601d588c43d
Anthony <dinhhung09138@gmail.com>
2018-07-27 05:41:44 +0700

    refs #4. Add view model for web user display

---
+++ TDH.Model/ViewModel/WebSite/CategoryViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TDH.Model.ViewModel.WebSite
+{
+    /// <summary>
+    /// Category view model
+    /// </summary>
+    public class CategoryViewModel : MetaViewModel
+    {
+        /// <summary>
+        /// The identifier
+        /// </summary>
+        public Guid ID { get; set; }
+
+        /// <summary>
+        /// Navigation identifier
+        /// </summary>
+        public Guid NavigationID { get; set; }
+
+        /// <summary>
+        /// Title
+        /// </summary>
+        public string Title { get; set; } = "";
+
+        /// <summary>
+        /// Alias
+        /// </summary>
+        public string Alias { get; set; } = "";
+
+        /// <summary>
+        /// Number of post
+        /// </summary>
+        public int Count { get; set; } = 0;
+
+        /// <summary>
+        /// List post by category id
+        /// </summary>
+        public List<PostViewModel> Posts { get; set; } = new List<PostViewModel>();
+    }
+}

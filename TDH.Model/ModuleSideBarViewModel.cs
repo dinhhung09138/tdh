d025d80b58cee6d8e30f33c7df4729bda8e74d75
Anthony <dinhhung09138@gmail.com>
2018-07-27 10:57:28 +0700

    Auto stash before merge of "develop" and "origin/develop"
    refs #4. add model

---
+++ TDH.Model/ModuleSideBarViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDH.Model
+{
+    /// <summary>
+    /// Module sidebar navigation view model
+    /// </summary>
+    public class ModuleSideBarViewModel
+    {
+        /// <summary>
+        /// Title
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Icon class
+        /// </summary>
+        public string Icon { get; set; }
+
+        /// <summary>
+        /// Default Url
+        /// </summary>
+        public string DefaultUrl { get; set; }
+
+    }
+}

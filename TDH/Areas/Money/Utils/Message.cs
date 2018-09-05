10f3261bcd332f6a8899256b6935b8874cc44aee
Anthony <dinhhung09138@gmail.com>
2018-09-05 10:29:48 +0700

    refs #5 Update message

---
+++ TDH/Areas/Money/Utils/Message.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TDH.Areas.Money.Utils
+{
+    public class Message
+    {
+        public static class Report
+        {
+            public static readonly string INDEX = "Báo cáo thống kê";
+        }
+
+        public static class Account
+        {
+            public static readonly string LIST = "Danh sách tài khoản";
+            public static readonly string HISTORY = "Lịch sử giao dịch tài khoản";
+            public static readonly string CREATE = "Thêm tài khoản";
+            public static readonly string EDIT = "Cập nhật tài khoản";
+        }
+
+        public static class AccountType
+        {
+            public static readonly string LIST = "Danh sách nhóm tài khoản";
+            public static readonly string CREATE = "Thêm nhóm tài khoản";
+            public static readonly string EDIT = "Cập nhật nhóm tài khoản";
+        }
+
+        public static class Category
+        {
+            public static readonly string LIST = "Danh sách danh mục thu chi";
+            public static readonly string HISTORY = "Lịch sử giao dịch";
+            public static readonly string CREATE = "Thêm danh mục thu chi";
+            public static readonly string EDIT = "Cập nhật danh mục thu chi";
+        }
+
+        public static class Group
+        {
+            public static readonly string LIST = "Danh sách quy tắc chi tiêu";
+            public static readonly string CREATE = "Thêm quy tắc chi tiêu";
+            public static readonly string SETTING = "Thiết lập quy tắc chi tiêu";
+            public static readonly string EDIT = "Cập nhật quy tắc chi tiêu";
+        }
+
+        public static class Flow
+        {
+            public static readonly string INDEX = "Lịch sử thu nhập - chi tiêu - Chuyển khoản";
+        }
+
+    }
+}
---
+++ TDH/Areas/Website/Utils/Message.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TDH.Areas.Website.Utils
+{
+    public class Message
+    {
+        public static class Navigation
+        {
+            public static readonly string LIST = "Danh mục bài viết";
+            public static readonly string CREATE = "Thêm danh mục bài viết";
+            public static readonly string EDIT = "Cập nhật danh mục bài viết";
+        }
+        public static class Category
+        {
+            public static readonly string LIST = "Chủ đề bài viết";
+            public static readonly string CREATE = "Thêm chủ đề bài viết";
+            public static readonly string EDIT = "Cập nhật chủ đề bài viết";
+        }
+        public static class News
+        {
+            public static readonly string LIST = "Danh sách bài viết";
+            public static readonly string CREATE = "Thêm bài viết";
+            public static readonly string EDIT = "Cập nhật bài viết";
+        }
+        public static class About
+        {
+            public static readonly string LIST = "Giới thiệu";
+        }
+        public static class Setting
+        {
+            public static readonly string NAVIGATION = "Danh mục hiển thị ở trang chủ";
+            public static readonly string CATEGORY = "Nhóm chủ đề hiển thị ở trang chủ";
+            public static readonly string CONFIGURATION = "Thiết lập chung";
+            public static readonly string EDIT_CONFIGURATION = "Cập nhật thông tin thiết lập";
+        }
+    }
+}

bfb9f13cadd4bd23912d96c76dcea5c0833aaa82
Anthony <dinhhung09138@gmail.com>
2018-09-06 13:53:17 +0700

    refs #5 PErsonal module

---
+++ TDH/Areas/Personal/Controllers/MeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TDH.Areas.Personal.Controllers
+{
+    [AllowAnonymous]
+    public class MeController : Controller
+    {
+        // GET: Personal/Me
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TDH/Areas/Personal/Utils/Message.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TDH.Areas.Personal.Utils
+{
+    public class Message
+    {
+    }
+}

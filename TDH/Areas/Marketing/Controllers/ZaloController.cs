16e5659909ec2f9dea8cacf29479089a4087b440
Anthony <dinhhung09138@gmail.com>
2018-09-17 11:04:15 +0700

    refs #5. Zalo ocntroller

---
+++ TDH/Areas/Marketing/Controllers/ZaloController.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TDH.Areas.Marketing.Controllers
+{
+    [AllowAnonymous]
+    public class ZaloController : Controller
+    {
+        // GET: Marketing/Zalo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

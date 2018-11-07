1c23e5f0d05a8b2de2b411da1739690b996caba3
Anthony <dinhhung09138@gmail.com>
2018-11-07 10:19:34 +0700

    remove webapi

--- TDH/Global.asax.cs
+++ TDH/Global.asax.cs
@@ -5,7 +5,6 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using System.Web.Http;
 using TDH.Common;
 using Utils;
 
@@ -16,7 +15,6 @@ namespace TDH
         protected void Application_Start()
         {
             //Must set it for web api
-            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
